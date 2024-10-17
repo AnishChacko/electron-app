0fcaba8e2a04edf6df417c83c915b359c1312b16
Anish Mathew Chacko <119020090+AnishChacko@users.noreply.github.com>
2024-10-17 21:28:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,28 @@
 const { app, BrowserWindow } = require('electron/main')
 const path = require('node:path')
 
+const { BaseWindow, WebContentsView } = require('electron')
+//const win = new BaseWindow({ width: 800, height: 400 })
+
 function createWindow () {
-  const win = new BrowserWindow({
+  const win = new BaseWindow({
     width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    height: 400,
+    title: 'MFE - Electron Shell'
   })
 
+const view1 = new WebContentsView()
+win.contentView.addChildView(view1)
+view1.webContents.loadURL('http://localhost:3000')
+view1.setBounds({ x: 0, y: 0, width: 400, height: 400 })
+
+const view2 = new WebContentsView()
+win.contentView.addChildView(view2)
+view2.webContents.loadURL('http://localhost:3001')
+view2.setBounds({ x: 400, y: 0, width: 400, height: 400 })
+
   //win.loadFile('index.html')
-  win.loadURL('http://localhost:3000');
+  //win.loadURL('http://localhost:3000');
 }
 
 app.whenReady().then(() => {
